5fc411ba3407654a68872b56d4290cc1bae45f5d
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-15 15:49:47 +0200

    learn about nesting with dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -125,3 +125,63 @@ for language in favorite_languages.values():
 for language in set(favorite_languages.values()):
     print(language.title())
 
+# A list of dictionaries
+alien_0 = {"color": "green", "points": 5}
+alien_1 = {"color": "yellow", "points": 10}
+alien_2 = {"color": "red", "points": 15}
+
+aliens = [alien_0, alien_1, alien_2]
+for alien in aliens:
+    print(alien)
+
+aliens = []
+for alien_number in range(30):
+    new_alien = {"color": "green", "points": 5, "speed": "slow"}
+    aliens.append(new_alien)
+
+for alien in aliens[:5]:
+    print(alien)
+
+print(f"The total number of aliens: {len(aliens)}.")
+
+for alien in aliens[:3]:
+    if alien["color"] == "green":
+        alien["color"] = "yellow"
+        alien["speed"] = "medium"
+        alien["points"] = 10
+
+print(aliens)
+
+# A list in a dictionary
+pizza = {
+    "crush": "thick",
+    "toppings": ["mushrooms", "extra cheese"]
+}
+
+print(f"You ordered a {pizza['crush']}-crust pizza "
+      "with the following toppings:")
+
+for topping in pizza["toppings"]:
+    print(f"\t{topping}")
+
+# A dictionary in a dictionary
+users = {
+    "aeinstein": {
+        "first": "albert",
+        "last": "einstein",
+        "location": "princeton"
+    },
+    "mcurie": {
+        "first": "marie",
+        "last": "curie",
+        "location": "paris"
+    }
+}
+
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info["location"]
+
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
