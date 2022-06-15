62d392429ca92ea73afb4c73b16962efc866d801
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-15 04:13:59 -0700

    complete exercises 6.4 - 6.6

--- exercises/chapter_6.py
+++ exercises/chapter_6.py
@@ -24,7 +24,7 @@ print(favorite_numbers["walie"])
 # 6.3 Glossary
 glossary = {
     "variables": "A variable is a label, which is used to reference a value associated with that label.",
-    "lists": "A list in Python, is a collection of values that are indexed, changeable, ordered, and can contain duplicates."
+    "lists": "A list in Python, is a collection of values that are indexed, changeable, ordered, and can contain duplicates.",
     "dictionaries": "A dictionary in Python, is a collection of key-value pairs which stores related data.",
     "loop": "A loop is a set of instructions that are repeated over and over until a certain condition evaluates to false.",
     "conditionals": "A conditional is an expression that evaluates to True/False."
@@ -36,11 +36,44 @@ print("\nDictionaries:", glossary["dictionaries"])
 print("\nLoop:", glossary["loop"])
 print("\nConditional:", glossary["conditionals"])
 
-# 6.4
+# 6.4 Glossary 2
+glossary["argument"] = "An argument is a way to provide information to a function."
+glossary["assignment operator"] = "An assignment operator is used to assign a value to a variable."
+glossary["program"] = "A program is an organized set of instructions, that performs a task when executed."
+glossary["boolean"] = "A boolean is a type of value that can be either True/False."
+glossary["constant"] = "A constant is a value that does not change throughout a programs life cycle."
 
-# 6.5
+for term, desc in glossary.items():
+    print(f"{term.title()}: ", desc)
 
-# 6.6
+# 6.5 Rivers
+rivers = {"nile": "egypt", "vaal": "south africa", "zembezi": "zambia"}
+
+for river, country in rivers.items():
+    print(f"The {river.title()} runs through {country.title()}.")
+
+print("The following rivers were mentioned:")
+for river in rivers.keys():
+    print(f"\t{river.title()}")
+
+print("The following rivers were mentioned:")
+for country in rivers.values():
+    print(f"\t{country.title()}")
+
+# 6.6 Polling
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "ruby",
+    "phil": "python"
+}
+people = ["chris", "edward", "gloria", "jen"]
+
+for name in favorite_languages.keys():
+    if name in people:
+        print(f"{name.title()}, thank you for taking the poll.")
+    else:
+        print(f"{name.title()}, please take the poll.")
 
 # 6.7
 
