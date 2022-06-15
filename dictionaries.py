599903ac5ab3baca796b659efe884c496fc8e5ed
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-15 04:13:18 -0700

    learn looping through dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -69,3 +69,59 @@ alien_0 = {"color": "green", "speed": "slow"}
 
 point_value = alien_0.get("points", "No point value assigned.")
 print(point_value)
+
+# Looping through all key-value pairs
+user_0 = {
+    "username": "efermi",
+    "first": "enrico",
+    "last": "fermi"
+}
+
+for key, value in user_0.items():
+    print(f"\nKey: {key}")
+    print(f"\nValue: {value}")
+
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "ruby",
+    "phil": "python"
+}
+
+for name, language in favorite_languages.items():
+    print(f"{name.title()}'s favorite language is {language.title()}.")
+
+# Looping through all the keys in a dictionary
+favorite_languages = {
+    "jen": "python",
+    "sarah": "c",
+    "edward": "ruby",
+    "phil": "python"
+}
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+friends = ["phil", "sarah"]
+for name in favorite_languages.keys():
+    print(name.title())
+
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f"\t{name.title()}, I see you love {language}!")
+
+if "erin" not in favorite_languages.key():
+    print("Erin, please take our poll!")
+
+# Looping through a dictionaries key in a particular order
+for name in sorted(favorite_languages.keys()):
+    print(f"{name.title()}, thank you for taking our poll.")
+
+# Looping through all values in a dictionary
+print("The following languages have been mentioned:")
+for language in favorite_languages.values():
+    print(language.title())
+
+for language in set(favorite_languages.values()):
+    print(language.title())
+
