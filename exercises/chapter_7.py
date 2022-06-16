e72f2130af3606e20731c51ba0e7efcb3172c234
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-16 20:09:37 +0200

    complete chapter 7 (user input and while loops)

--- exercises/chapter_7.py
+++ exercises/chapter_7.py
@@ -2,7 +2,7 @@
 rental_car = input("\nWhat kind of car would you like to rent?")
 print(f"\nLet me see if I can find you a {rental_car}.")
 
-# 7.2 Restaurant seating
+# # 7.2 Restaurant seating
 group_size = input("\nHow many people are in your dinner group? ")
 group_size = int(group_size)
 
@@ -16,21 +16,131 @@ number = input("\nTell me a number and I will tell you "
                "if its a mulple of 10 or not: ")
 number = int(number)
 
-if number % 10 == 0:
-    print("The number is a multiple of 10.")
+if 10 % number == 0:
+    print(f"The number {number} is a multiple of 10.")
 else:
-    print("The number is Not a multiple of 10.")
+    print(f"The number {number} is Not a multiple of 10.")
 
 # 7.4 Pizza toppings
+prompt = "\nPlease tell me what topping you would like on your pizza:"
+prompt += "\n(Enter 'quit' when you're finished.) "
+
+active = True
+requested_toppings = []
+
+while active:
+    requested_topping = input(prompt)
+
+    if requested_topping == "quit":
+        active = False
+    else:
+        print(f"\tAdding {requested_topping}...")
+        requested_toppings.append(requested_topping)
+
+print("\nYour pizza is ready!")
+if requested_toppings:
+    print("Your pizza has the following toppings:")
+    for topping in requested_toppings:
+        print("\t" + topping)
 
 # 7.5 Movie tickets
+prompt = "\nPlease tell how old you are:"
+prompt += "\n(Enter 'quit' when you're finished.) "
+
+active = True
+
+while active:
+    age = int(input(prompt))
+
+    if age == "quit":
+        active = False
+
+    if age < 3:
+        print("Your ticket is free.")
+    elif age <= 12:
+        print("Your ticket will cost $10.")
+    elif age > 12:
+        print("Your ticket will cost $15.")
 
 # 7.6 Three exits
+prompt = "\nPlease tell me what topping you would like on your pizza:"
+prompt += "\n(Enter 'quit' when you're finished.) "
+
+active = True
+requested_toppings = []
+
+while active:
+    requested_topping = input(prompt)
+
+    if requested_topping == "quit":
+        break
+    if len(requested_toppings) >= 10:
+        active = False
+        print("We can only allow a maximum of 10 toppings per pizza.")
+
+    print(f"\nAdding {requested_topping}...")
+    requested_toppings.append(requested_topping)
+
+print("\nYour pizza is ready!")
+if requested_toppings:
+    print("Your pizza has the following toppings:")
+    for topping in requested_toppings:
+        print("\t" + topping)
 
 # 7.7 Infinity
+#! The following code runs forever
+# while True:
+#     requested_topping = input(prompt)
+#     print(f"\nAdding {requested_topping}...")
 
 # 7.8 Deli
+sandwich_orders = ["cheese", "pastrami", "chicken"]
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+
+    print(f"\nMaking {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\nThe following sandwiches were made:")
+for sandwich in finished_sandwiches:
+    print("\t" + sandwich)
 
 # 7.9 No pastrami
+sandwich_orders = ["cheese", "pastrami",
+                   "chicken", "pastrami", "beef", "pastrami"]
+print("\nWe have ran out of pastrami.")
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+
+    if current_sandwich == "pastrami":
+        sandwich_orders.remove("pastrami")
+        continue
+    else:
+        print(f"\nMaking a {current_sandwich} sandwich.")
+
+print("\nThe following sandwiches were made:")
+for sandwich in finished_sandwiches:
+    print("\t" + sandwich)
 
 # 7.10 Dream Vacation
+polling_active = True
+dream_vacations = {}
+
+while polling_active:
+    name = input("\nWhat is your name? ")
+    place = input(
+        "If you could visit one place in the world, where would you go? ")
+
+    dream_vacations[name] = place
+
+    repeat = input("\nWould you like to let another person respond? (yes/no) ")
+    if repeat == "no":
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name, place in dream_vacations.items():
+    print(f"\t{name.title()} would like to visit {place.title()}.")
--- user_input_while_loops.py
+++ user_input_while_loops.py
@@ -37,3 +37,109 @@ if number % 2 == 0:
     print(f"\nThe number {number} is even.")
 else:
     print(f"\nThe number {number} is odd.")
+
+# The while loop in action
+current_number = 1
+while current_number <= 5:
+    print(current_number)
+    current_number += 1
+
+# Let the user choose when to quit
+prompt = "\nTell me something and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+
+while message != "quit":
+    message = input(prompt)
+
+    if message != "quit":
+        print(message)
+
+# Using a flag
+prompt = "\nTell me something and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+
+while active:
+    message = input(prompt)
+
+    if message == "quit":
+        active = False
+    else:
+        print(message)
+
+# Using break to exit a loop
+prompt = "\nPlease enter the name of a city that you have visited:"
+prompt += "\n(Enter 'quit' when you're finished.)"
+
+while True:
+    city = input(prompt)
+
+    if city == "quit":
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+
+# Using continue in a loop
+current_number = 0
+
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+
+    print(current_number)
+
+# Avoiding infinite loops
+x = 1
+while x <= 5:
+    print(x)
+    x += 1
+
+#! This loop runs forever
+x = 1
+while x <= 5:
+    print(x)
+
+# Moving items from one list to another
+unconfirmed_users = ["alice", "brian", "candice"]
+confirmed_users = []
+
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+
+    print(f"\nVerifying user: {current_user.title()}")
+    confirmed_users.append(current_user)
+
+print("\nThe following users have been confirmed:")
+for user in confirmed_users:
+    print("\t" + user.title())
+
+# Removing all instances of specific values from a list
+pets = ["dog", "cat", "dog", "goldfish", "cat", "rabbit", "cat"]
+print(pets)
+
+while "cat" in pets:
+    pets.remove("cat")
+
+print(pets)
+
+# Filling a dictionary with user input
+responses = {}
+polling_active = True
+
+while polling_active:
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    responses[name] = response
+
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == "no":
+        polling_active = False
+
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name.title()} would like to climb {response}.")
