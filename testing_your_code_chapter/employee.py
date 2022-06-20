d87c69edbbb92a6d3ebae5367883389f73b42fa1
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-20 14:23:03 +0200

    complete chapter 11 (testing your code)

--- testing_your_code_chapter/employee.py
+++ testing_your_code_chapter/employee.py
@@ -1,1 +1,12 @@
-# Code will come here
+class Employee:
+    """A simple attempt to model an employee."""
+
+    def __init__(self, first, last, annual_salary):
+        """Initialize employee attributes."""
+        self.first = first
+        self.last = last
+        self.annual_salary = annual_salary
+
+    def give_raise(self, raise_amount=5_000):
+        """Increment annual_salary by 5000 if amount not given."""
+        self.annual_salary += raise_amount
--- testing_your_code_chapter/test_employee.py
+++ testing_your_code_chapter/test_employee.py
@@ -1,1 +1,30 @@
 import unittest
+from employee import Employee
+
+
+class TestEmployee(unittest.TestCase):
+    """Tests for the Employee class."""
+
+    def setUp(self) -> None:
+        """
+        Create an Employee instance
+        """
+        self.my_employee = Employee("bob", "willis", 35_000)
+
+    def test_give_default_raise(self):
+        """
+        Test if annual_salary is raised correctly by default amount.
+        """
+        self.my_employee.give_raise()
+        self.assertEqual(40_000, self.my_employee.annual_salary)
+
+    def test_give_custom_raise(self):
+        """
+        Test if annual_salary is raised correctly by the given amount.
+        """
+        self.my_employee.give_raise(3_000)
+        self.assertEqual(38_000, self.my_employee.annual_salary)
+
+
+if __name__ == "__main__":
+    unittest.main()
