407441267f5cb7bb04ab57573d0b2aa0c46a8601
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-22 16:00:35 +0200

    add functionality to the ship to fire bullets

--- projects/alien_invasion_game/ship.py
+++ projects/alien_invasion_game/ship.py
@@ -7,8 +7,8 @@ class Ship:
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
@@ -27,9 +27,9 @@ class Ship:
     def update(self):
         """Update the ship's position based on the movement flag."""
         if self.moving_right and self.rect.right < self.screen_rect.right:
-            self.x = self.settings.ship_speed
+            self.x += self.settings.ship_speed
         if self.moving_left and self.rect.left > 0:
-            self.x = self.settings.ship_speed
+            self.x -= self.settings.ship_speed
 
         self.rect.x = self.x
 
