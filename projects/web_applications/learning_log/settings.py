c4413545b990da91a17b62334ae3d3cfd5a9ce1c
Zaid Marrie <zaidmarrie27@gmail.com>
2022-06-30 14:43:42 +0200

    add secret key to .env

--- projects/web_applications/learning_log/settings.py
+++ projects/web_applications/learning_log/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import django_heroku
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bo2!-yy=057pc$%rq-rtzb_3lkex36((wicn_+-bdr7rhgmzl6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
